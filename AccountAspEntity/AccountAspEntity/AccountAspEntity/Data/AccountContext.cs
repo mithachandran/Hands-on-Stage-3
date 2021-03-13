936b0651cb67910ea83c1e090912c46b779b2269
mithachandran <chandranmitha@gmail.com>
2021-03-13 18:29:04 +0530

    message

---
+++ AccountAspEntity/AccountAspEntity/AccountAspEntity/Data/AccountContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccountAspEntity.Data
+{
+    public class Accountcontext : DbContext
+    {
+        public Accountcontext(DbContextOptions<Accountcontext> options) : base(options)
+
+        {
+        }
+
+        public DbSet<AccountAspEntity.Models.Account> Account { get; set; }
+    }
+}
---
+++ GenericMedicine/GenericMedicine/GenericMedicine.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GenericMedicine.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GenericMedicine.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cc23180b-68ad-42e8-bd43-147608d93a2c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
